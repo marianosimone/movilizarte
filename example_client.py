c90e557a4d731779342f39306cb0d3c7701a9ae9
Mariano Simone <marianosimone@gmail.com>
2013-07-19 23:13:25 -0300

    adapt example client

--- example_client.py
+++ example_client.py
@@ -4,6 +4,7 @@ s = socket.socket()
 host = socket.gethostname()
 port = 5204
 s.connect((host, port))
+s.send("400,200\n")
 for i in xrange(1, 200):
     msg = "%d,%d,3\n" % (i*2, i)
     s.send(msg)
